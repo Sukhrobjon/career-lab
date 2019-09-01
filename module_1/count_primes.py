0deeb8ff8b999ab58e945f51526c13e100a507b9
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-08-31 23:13:35 -0700

    [added] docstring and passed all test on repl

--- module_1/count_primes.py
+++ module_1/count_primes.py
@@ -5,11 +5,34 @@ https: // repl.it/student/submissions/7695405
 
 
 def is_prime(n):
+    """Determine if input number is prime number
+    Args:
+        n(int): input number
+    Return:
+        true or false(bool):
+    """
+    # 1 is not prime
+    if n == 1:
+        return False
+
     for curr_num in range(2, n):
         # if input is evenly divisible by the current number
         if n % curr_num == 0:
+            # print("current num:", curr_num)
             return False
     return True
 
-n = 10
-print(is_prime(7))
+
+def count_primes(n):
+    """Counts number of prime numbers less than a non-negative number, n"""
+    count = 0
+    for curr_num in range(2, n):
+        # check if current number is prime
+        if is_prime(curr_num):
+            count += 1
+    return count
+
+n = 2
+m = 1
+print("is prime:", is_prime(m))
+print("num of primes:", count_primes(m))
