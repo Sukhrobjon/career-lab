4c4a6c254b4c6e582c095ce533fddcf0b27dd7db
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-06 23:50:06 -0700

    [added] problem prompt

--- excel_column.py
+++ excel_column.py
@@ -1,3 +1,17 @@
+"""
+A -> 1   
+B -> 2  
+C -> 3
+... 
+Z -> 26
+AA -> 27
+AB -> 28
+Prompt
+Given a column title as appears in an Excel sheet, return its corresponding
+column number.
+"""
+
+
 def excel_column_to_number(column):
     """
     """
