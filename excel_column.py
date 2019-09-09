b8ed1b5ff629b6f87e77f7e16f23c58ec49df099
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-09 09:47:26 -0700

    [cleaned] up the code

--- excel_column.py
+++ excel_column.py
@@ -1,6 +1,6 @@
 """
-A -> 1   
-B -> 2  
+A -> 1
+B -> 2
 C -> 3
 Z -> 26
 AA -> 27
@@ -9,21 +9,21 @@ Prompt
 Given a column title as appears in an Excel sheet, return its corresponding
 column number.
 """
+import string
 
 
 def excel_column_to_number(column):
     """Converts a string(column title) to number representation"""
+
     offset = 65     # capital 'A' ascii value
-    # iterate throurgh the title
-    # for each character
-    # add the value by multiplying the base
     value = 0
     BASE = 26
     for _, char in enumerate(column):
+        if char not in string.ascii_letters:
+            raise ValueError("String should be ascii letter!")
         char_val = ord(char.upper()) - offset
-        print(char)
         value = BASE * value + 1 + char_val
     return value
 
 
-print(excel_column_to_number("AAb"))
+print(excel_column_to_number("Ab"))
