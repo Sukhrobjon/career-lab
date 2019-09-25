cade103fe91509fbdd84d18dd473524e5b12cb55
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-25 10:31:52 -0700

    [finished] merge overlapping challenge and passed tests

--- module_3/merge_overlapping_intervals.py
+++ module_3/merge_overlapping_intervals.py
@@ -1,8 +1,9 @@
 from functools import reduce
 
 
-def merge_overlapping_intervals(intervals):
+def merge_overlapping_intervals_v1(intervals):
     """
+    NOTE: DOES NOT WORK! BUT KEEP IT FOR FUTURE REFERENCE
     Given a collection of intervals, merge all overlapping intervals.
     Return them in a sorted order
     """
@@ -19,14 +20,46 @@ def merge_overlapping_intervals(intervals):
         if merged_array[j][1] <= intervals[i][1] and merged_array[j][1] >= intervals[i][0]:
             print("inside if")
             print(f"merged: {merged_array[j]}, intervals: {intervals[i]}")
+            # merged_array.pop()
             merged_array.append([merged_array[j][0], intervals[i][1]])
         else:
             merged_array.append(intervals[i])
+
         j += 1
     return merged_array
 
 
-# arr = [[1, 3], [2, 6], [8, 10], [15, 18]]
-arr = [[2, 6], [1, 3], [15, 18], [16, 20]]
+def merge_overlapping_intervals_v2(intervals):
+    """
+    Given a collection of intervals, merge all overlapping intervals.
+    Return them in a sorted order
+    """
+    # for interval in intervals:
+    # check if the the number at the index of 1 of each interval
+    # if so add the interval[0] and next interval[1] to the new array
 
-print(merge_overlapping_intervals(arr))
+    if len(intervals) == 0:
+        return []
+    intervals = sorted(intervals)  # O(nlog(n))
+    # to hold the final values
+    merged_stack = [intervals[0]]
+
+    j = 0
+    for i in range(1, len(intervals)):
+        # check if the interval in the merged stack and intervals are
+        # overlapping
+        if merged_stack[j] < intervals[i] and merged_stack[j][1] >= intervals[i][0]:
+            curr_item = merged_stack.pop(j)
+            # print("last item: ", curr_item)
+            merged_stack.append([curr_item[0], intervals[i][1]])
+        # otherwise, add the current interval from intervals to merged stack
+        else:
+            merged_stack.append(intervals[i])
+            j += 1
+    return merged_stack
+
+
+arr = [[1, 3], [2, 6], [8, 10], [15, 18]]  # => [[1,6], [8,10], [15,18]]
+# arr = []
+
+print(merge_overlapping_intervals_v2(arr))
