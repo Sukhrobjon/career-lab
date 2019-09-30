635d053935c0f7806dfa22ffd8273780585431db
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-29 22:29:01 -0700

    [implemented] wave array

--- module_2/power_of_three.py
+++ module_2/power_of_three.py
@@ -1,9 +1,10 @@
 def power_of_three(n):
-    """Determine if number n is power is three
+    """
+    Determine if number n is power is three
     Returns True if n is power of three, False otherwise
     """
     # devide the number by 3 until
-    # it is not devisible and equal to 1
+    # it is not devisible and the quotient is equal to 1
     quotient = 0
 
     while quotient != 1:
@@ -17,4 +18,4 @@ def power_of_three(n):
     return True
 
 
-print(power_of_three(0))
+print(power_of_three(1162261467))
--- module_3/wave_array.py
+++ module_3/wave_array.py
@@ -2,15 +2,20 @@ def wave_array(arr):
     """
     Given an array of integers, sort the array into a wave like array and
     return it, In other words, arrange the elements into a sequence such
-    that a[1] >= a[2] <= a[3] >= a[4] <= a[5]
+    that a[0] >= a[1] <= a[2] >= a[3] <= a[4]
     """
 
-    for i in range(len(arr)-1, 2):
-        print(arr[i])
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    for i in range(0, len(arr), 2):
+        # if current even element is smaller than previous, swap
+        if i > 0 and arr[i] < arr[i-1]:  # skip the 0th element
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+
+        # if current even element is smaller than next
+        if i < (len(arr) - 1) and arr[i] < arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
 
     return arr
 
-arr = [1, 2, 3, 4, 5]
+arr = [1, 2, 3, 4, 5, 6]
 
 print(wave_array(arr))
