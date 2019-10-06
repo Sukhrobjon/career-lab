4cc858cdc1078a47673634af35974f08c8e171f5
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-06 16:38:36 -0700

    [finished] merge two sorted list

--- module_5/merge_sorted_lists.py
+++ module_5/merge_sorted_lists.py
@@ -3,37 +3,62 @@ def merge_sorted_list(arr1, arr2):
     # since we know arr1 is always greater or equal to (m+n)
     # we compare arr2[i] with arr1[j] element and check if it
     # should be placed there
-    
     i, j = 0, 0
-    while i < len(arr1) - 1:
-        if j > len(arr2) - 1:
-            break
-        else:
-            if arr2[j] == arr1[i] or arr2[j] < arr1[i+1]:
-                arr1.insert(i+1, arr2[j])
-                arr1.pop()
-                i += 2
-                j += 1
-            elif arr2[j] > arr1[i]:
-                if arr1[i+1] == 0:
-                    arr1[i+1] = arr2[j]
-                    i += 1
-                    j += 1
-                else:
-                    i += 1
-            elif arr2[j] < arr1[i]:
-                arr1.insert(i, arr2[j])
-                arr1.pop()
+    while i < len(arr1) and j < len(arr2):
+
+        if arr2[j] == arr1[i] or arr2[j] <= arr1[i+1]:
+            arr1.insert(i+1, arr2[j])
+            arr1.pop()
+            i += 2
+            j += 1
+        elif arr2[j] > arr1[i]:
+            if arr1[i+1] == 0:
+                arr1[i+1] = arr2[j]
                 i += 1
                 j += 1
-            
-            print(arr1)
+            else:
+                i += 1
+        elif arr2[j] < arr1[i]:
+            arr1.insert(i, arr2[j])
+            arr1.pop()
+            i += 1
+            j += 1
+        print(arr1)
     return arr1
 
+def merge_sorted_list_v2(nums1, m, nums2, n):
+    # fill up that empty space with string
+    stop = 0
+    for i in range(len(nums1)-1, 0, -1):
+        if stop == len(nums2):
+            break
+        else:
+            nums1[i] = ""
+            stop += 1
+    print(nums1)
+    if m == 0:
+        nums1 = nums2
+    
+    i, j = 0, 0
 
+    empty_space = len(nums2)
+    while j < len(nums2):
+        if nums2[j] <= nums1[i] or i == empty_space:
+            nums1.insert(i, nums2[j])
+            nums1.pop()
+            j += 1
+            empty_space += 1
+        else:
+            i += 1
+
+    return nums1
 a = [1, 2, 3, 0, 0, 0]
-b = [1, 2, 3, 3, 4, 6]
+b = [2, 5, 6]
 
-result = merge_sorted_list(a, b)
+nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0]
+m = 6
+nums2 = [1, 2, 2]
+n = 3
+result = merge_sorted_list_v2(nums1, 6, nums2, 3)
 
 print("result: ", result)
--- module_5/rotated_array.py
+++ module_5/rotated_array.py
@@ -1,26 +1,63 @@
 def find_pivot_index(nums):
     """
-    Find the min num in the rotated sorted array.
+    Suppose a sorted array A is rotated at some pivot unknown to you
+    beforehand. (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+    Find the minimum element. NOTE: The array will not contain duplicates.
     """
     min_num = nums[0]
     pivot_index = 0
     left = 0
     right = len(nums) - 1
-    # i = 0
+    if left == right:
+        return pivot_index, nums[pivot_index]
 
     while left <= right:
         mid = (left + right) // 2
         print(nums[mid])
-        if min_num < nums[mid]:
-            left = mid + 1
-        elif min_num > nums[mid]:
+    
+        if min_num > nums[mid]:
             min_num = nums[mid]
             pivot_index = mid
             right = mid - 1
+        else:
+            left = mid + 1
 
     return pivot_index, min_num
 
-def search
-nums = [2, 2, 4, 5, 7, 1]
-result = find_pivot_index(nums)
+
+def find_pivot_index_in_duplicates(nums):
+    """
+    Suppose a sorted array A is rotated at some pivot unknown to you
+    beforehand. (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+    Find the minimum element. NOTE: The array will contain duplicates.
+    """
+    min_num = nums[0]
+    pivot_index = 0
+    left = 0
+    right = len(nums) - 1
+    if left == right:
+        return pivot_index, nums[pivot_index]
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        print(nums[mid], mid)
+
+        if min_num > nums[mid]:
+            min_num = nums[mid]
+            pivot_index = mid
+            right = mid - 1
+        # elif min_num == nums[mid]:
+        #     # continue
+
+        else:
+            left = mid + 1
+
+    return pivot_index, min_num
+
+
+# nums = [4, 5, 6, 7, 0, 1, 2]
+nums = [10, 10, 10, 10, 1]
+single = [5, 5, 5, 5, 3]
+result = find_pivot_index_in_duplicates(nums)
 print(result)
