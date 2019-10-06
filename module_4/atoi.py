a87b0231e98e2d3e6b1be101fdec211af15604ba
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-10-05 20:21:41 -0700

    [finished] atoi

--- module_4/atoi.py
+++ module_4/atoi.py
@@ -1,35 +1,33 @@
-def atoi(text):
-    """
-    Converts string into int if it is convertible, otherwise returns 0
-    """
-    result = ""
-    INT_MAX = (2**31)-1
-    INT_MIN = -(2**31)
-    sign = "+-"
-    start = 0
-    for i, char in enumerate(text):
-        if not char.isspace() and char in sign:
-            if char == "-" or char.isdigit():
-                result += char
-                start = i
+class Solution(object):
+    def atoi(self, text):
+        """Converts string into int if it is convertible, otherwise returns 0"""
+        
+        INT_MAX = (2**31)-1
+        INT_MIN = -(2**31)
+
+        text = text.strip()
+        
+        if text[0].isalpha():
+            # print(text)
+            return 0
+
+        result = text[0]
+
+        for i in range(1, len(text)):
+            char = text[i]
+            if not char.isdigit():
                 break
-    # check if the first char after +- sign is number
-    if not text[start+1].isdigit():
-        return 0
-    
-    for i in range(start, len(text)):
-        print("num starts:", text[start:])
-        char = text[i]
-        if char.isdigit():
-            print(char)
+            # otherwise
             result += char
-            print("inside isdigit: ", result)
+
+        if INT_MIN <= int(result) <= INT_MAX:
+            return int(result)
         else:
-            break
-    print("before return")
-    return int(result)
+            return INT_MIN
 
-
-text = "-4193 with words"
-result = atoi(text)
-print(result)
+text = ["42", "   -42", "-4193 with words", "words and 987", "-91283472332"]
+# word = "987  "
+obj = Solution()
+for word in text:    
+    result = obj.atoi(word)
+    print(result)
