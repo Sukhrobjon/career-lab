d9538dda0d552c0a107de793eeb44c4a959ed801
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2020-02-10 12:42:48 -0800

    [reorganizing] yelp question

--- online_interview_questions/yelp_online_problem/num_of_unique_chains.py
+++ online_interview_questions/yelp_online_problem/num_of_unique_chains.py
@@ -12,27 +12,47 @@ class Business():
         return (f"name: {self.name}, location: {self.location}, id: {self._id}")
 
 
-def get_unique_business_id():
+def get_unique_business_id(buss_list):
     """
     Returns only businesses with unique id
     """
+    seen_id = set()
+    for i, buss in buss_list:
+        if buss._id not in seen_id:
+            seen_id.add(buss._id)
+        else:
+            buss_list.pop(i)
+
+    return buss_list
+
+
+def get_chains_at_location(businesses, location):
+    """
+    Return number of chains at given location
+    """
     pass
-def unique_chains(self, buss_obj, location):
+
+
+def unique_chains(buss_obj, location):
     """Counts the unique number of chains in the given location"""
     seen_id = set()
     chains = {}
     for buss in buss_obj:
+        # first filter by location
         if location == buss.location:
+            # filter by unique business id
             if buss._id not in seen_id:
                 seen_id.add(buss._id)
+                # count the chains
                 if buss.name not in chains:
                     chains[buss.name] = 1
                 else:
                     chains[buss.name] += 1
     chains = list(map(list, chains.items()))
+    # sort the chains by number of stores and alphabetically
     return sorted(chains, key=lambda x: (-x[1], x[0]))
 
-
+def count_unique_id(businesses, location)
 businesses = [
             Business("Sturbucks", "SF", 100),
             Business("Sturbucks", "SF", 100),
@@ -42,10 +62,11 @@ businesses = [
             Business("Amazon", "SF", 110),
             Business("McDonalds", "SF", 104),
             Business("McDonalds", "SF", 104),
-            Business("McDonalds", "NYC", 106)
+            Business("McDonalds", "NYC", 106),
+            Business("McDonalds", "NYC", 105)
             ]
 
-obj = Business()
+# obj = Business()
 
-result = obj.unique_chains(businesses, "SF")
+result = unique_chains(businesses, "NYC")
 print(result)
